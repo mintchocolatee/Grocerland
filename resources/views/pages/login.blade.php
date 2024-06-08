85ac3f997722e048dd00e8b74daffd8f5656f5a5
shumin <shumin1104@gmail.com>
2024-06-08 21:51:23 +0800

    added modal for fail to add to cart

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -28,4 +28,26 @@
             @endif
         </div>
     </div>
+    @if (session('success'))
+        <div class="modal-overlay">
+            <div class="modal-content">
+                <h2>Success!</h2>
+                <p>{{ Session::get('success') }}</p>
+                <button id="close-btn">Close</button>
+            </div>
+        </div>
+    @elseif (session('error'))
+        <div class="modal-overlay">
+            <div class="modal-content">
+                <h2>Fail to add!</h2>
+                <p>{{ Session::get('error') }}</p>
+                <button id="close-btn">Close</button>
+            </div>
+        </div>
+    @endif
+    <script>
+        document.getElementById('close-btn').onclick = function() {
+            window.location.href = "{{ route('user.login') }}";
+        };
+    </script>
 @endsection
